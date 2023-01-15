de99f98c571f29ea6ad2bdfc658b710e301db3f9
Ivan <shuvalol.vana@gmail.com>
2023-01-15 19:05:00 +0200

    task 4 complited

---
+++ task4/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+
+if (num%2!=1){
+    while(num>2){
+        num=num-2;
+        Console.Write($"{num} ");
+    }
+}
+else{
+    num=num-1;
+    Console.Write($"{num} ");
+    while(num>2){
+        num=num-2;
+        Console.Write($"{num} ");
+    }
+}
