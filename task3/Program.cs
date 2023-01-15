dc09fd8323a181bee03590430870e4cacb83ca67
Ivan <shuvalol.vana@gmail.com>
2023-01-15 18:58:57 +0200

    task 3 complited

---
+++ task3/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+
+if (num%2!=1){
+    Console.WriteLine("Да, число четное");
+}
+else{
+    Console.WriteLine("Нет, число нечетное");
+}
