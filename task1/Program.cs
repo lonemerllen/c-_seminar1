ed60cbb6ff708be34aa0de4e8f95b0aa6b7b17aa
Ivan <shuvalol.vana@gmail.com>
2023-01-15 17:17:38 +0200

    task 1 complited

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,1 +1,14 @@
-﻿Console.Write(2+2);
+﻿Console.Write("Введите первое число: ");
+int num1 = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int num2 = int.Parse(Console.ReadLine());
+int max = num1, min = num1;
+if(num1>num2){
+    max = num1;
+    min = num2;
+}
+else{
+    max = num2;
+    min = num1;
+}
+Console.WriteLine($"max = {max}, min = {min}");
