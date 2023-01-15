996833d348dccc18126158b7bee8a82395464222
Ivan <shuvalol.vana@gmail.com>
2023-01-15 18:50:30 +0200

    task 2 complited

---
+++ task2/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите первое число: ");
+int num1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int num2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите третье число: ");
+int num3 = int.Parse(Console.ReadLine());
+int max = num1;
+
+if (num1>num2){
+    max = num1;
+}
+else{
+    max =num2;
+}
+if (max>num3){
+    max=max;
+}
+else{
+    max = num3;
+}
+Console.WriteLine($"max = {max}");
